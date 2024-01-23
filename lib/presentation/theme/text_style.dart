f77b713eb353f5aa9770d279fc9c364c926d459b
Codename26 <slava.burchenko@gmail.com>
2024-01-23 14:17:59 +0100

    Move text style to external file

---
+++ lib/presentation/theme/text_style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const text20Bold = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+const text20BoldWhite =
+    TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white);
